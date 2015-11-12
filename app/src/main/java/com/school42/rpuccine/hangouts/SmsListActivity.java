3863705daadca219a37e15bf4582880605fd2a07
5H55 <5h55@Air-de-5H55.home>
2015-11-12 15:27:27 +0100

    sms

--- app/src/main/java/com/school42/rpuccine/hangouts/SmsListActivity.java
+++ app/src/main/java/com/school42/rpuccine/hangouts/SmsListActivity.java
@@ -11,8 +11,13 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SmsListActivity extends AppCompatActivity {
 
@@ -21,12 +26,14 @@ public class SmsListActivity extends AppCompatActivity {
     static final String EXTRA_PHONE = "com.school42.rpuccine.hangouts.phone";
 
     final String[] col = {Telephony.TextBasedSmsColumns.ADDRESS,
+            Telephony.TextBasedSmsColumns.TYPE,
             Telephony.TextBasedSmsColumns.DATE,
             Telephony.TextBasedSmsColumns.BODY};
 
     final Uri smsUri = Telephony.Sms.CONTENT_URI;
 
     String selection = Telephony.TextBasedSmsColumns.ADDRESS + "=";
+    String order = Telephony.TextBasedSmsColumns.DATE + " ASC";
     String num;
 
     @Override
@@ -42,9 +49,7 @@ public class SmsListActivity extends AppCompatActivity {
         Bundle extra = i.getExtras();
         num = extra.getString(EXTRA_PHONE);
 
-        //selection = selection + num;
-        selection = selection + "'+33661684346'";
-
+        selection = selection + "'" + num + "'";
 
         // Test permission et request de celle-ci si non.
         if (ContextCompat.checkSelfPermission(this,
@@ -61,41 +66,12 @@ public class SmsListActivity extends AppCompatActivity {
                     smsUri,
                     col,
                     selection,
-                    //null,
                     null,
-                    Telephony.Sms.DEFAULT_SORT_ORDER);
+                    order);
 
-            LinearLayout container = (LinearLayout) findViewById(R.id.smsListContainer);
-            TextView num = new TextView(this);
-            TextView date = new TextView(this);
-            TextView body = new TextView(this);
-
-            if (resCursor.moveToFirst()) {
-                //resCursor.moveToFirst();
-                num.setText(resCursor.getString(resCursor.getColumnIndex(Telephony.TextBasedSmsColumns.ADDRESS)));
-                date.setText(resCursor.getString(resCursor.getColumnIndex(Telephony.TextBasedSmsColumns.DATE)));
-                body.setText(resCursor.getString(resCursor.getColumnIndex(Telephony.TextBasedSmsColumns.BODY)));
-
-                num.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                        LinearLayout.LayoutParams.WRAP_CONTENT));
-                date.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                        LinearLayout.LayoutParams.WRAP_CONTENT));
-                body.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                        LinearLayout.LayoutParams.WRAP_CONTENT));
-
-                container.addView(num);
-                container.addView(date);
-                container.addView(body);
-
-                resCursor.close();
-            }
-            else {
-                TextView error = new TextView(this);
-                error.setText(selection);
-                error.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                        LinearLayout.LayoutParams.WRAP_CONTENT));
-                container.addView(error);
-            }
+            // Construction et remplissage des sms
+            buildSmsView(resCursor);
+            resCursor.close();
         }
     }
 
@@ -112,42 +88,12 @@ public class SmsListActivity extends AppCompatActivity {
                             smsUri,
                             col,
                             selection,
-                            //null,
                             null,
-                            Telephony.Sms.DEFAULT_SORT_ORDER);
+                            order);
 
-                    LinearLayout container = (LinearLayout) findViewById(R.id.smsListContainer);
-                    TextView num = new TextView(this);
-                    TextView date = new TextView(this);
-                    TextView body = new TextView(this);
-
-                    if (resCursor.moveToFirst()) {
-                        //resCursor.moveToFirst();
-                        num.setText(resCursor.getString(resCursor.getColumnIndex(Telephony.TextBasedSmsColumns.ADDRESS)));
-                        date.setText(resCursor.getString(resCursor.getColumnIndex(Telephony.TextBasedSmsColumns.DATE)));
-                        body.setText(resCursor.getString(resCursor.getColumnIndex(Telephony.TextBasedSmsColumns.BODY)));
-
-                        num.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                                LinearLayout.LayoutParams.WRAP_CONTENT));
-                        date.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                                LinearLayout.LayoutParams.WRAP_CONTENT));
-                        body.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                                LinearLayout.LayoutParams.WRAP_CONTENT));
-
-                        container.addView(num);
-                        container.addView(date);
-                        container.addView(body);
-
-                        resCursor.close();
-                    }
-                    else {
-                        TextView error = new TextView(this);
-                        error.setText("No contact sms found second");
-                        error.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                                LinearLayout.LayoutParams.WRAP_CONTENT));
-                        container.addView(error);
-                    }
-
+                    // Construction et remplissage des sms
+                    buildSmsView(resCursor);
+                    resCursor.close();
                 }
                 else {
 
@@ -164,5 +110,71 @@ public class SmsListActivity extends AppCompatActivity {
             }
 
         }
+    }
+
+    private void buildSmsView(Cursor resCursor) {
+
+        LinearLayout smsListContainer = (LinearLayout) findViewById(R.id.smsListContainer);
+        int nb_sms = 0;
+
+        while (resCursor.moveToNext()) {
+
+            // Creation du smsContainer et de ses textView
+            LinearLayout smsContainer = new LinearLayout(this);
+            TextView numView = new TextView(this);
+            TextView typeView = new TextView(this);
+            TextView dateView = new TextView(this);
+            TextView bodyView = new TextView(this);
+
+            // Set le champs des textView du sms
+            numView.setText(resCursor.getString(resCursor.getColumnIndex(Telephony.TextBasedSmsColumns.ADDRESS)));
+            typeView.setText(resCursor.getString(resCursor.getColumnIndex(Telephony.TextBasedSmsColumns.TYPE)));
+            Date date_mili = new Date(resCursor.getLong(resCursor.getColumnIndex(Telephony.TextBasedSmsColumns.DATE)));
+            String formattedDate = new SimpleDateFormat("dd/MM/yyyy HH:mm").format(date_mili);
+            dateView.setText(formattedDate);
+            bodyView.setText(resCursor.getString(resCursor.getColumnIndex(Telephony.TextBasedSmsColumns.BODY)));
+
+            // Set color des textView
+            numView.setTextColor(getResources().getColor(R.color.red));
+            dateView.setTextColor(getResources().getColor(R.color.red));
+            bodyView.setTextColor(getResources().getColor(R.color.black));
+
+            // Set les params des textView
+            LinearLayout.LayoutParams textViewParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            numView.setLayoutParams(textViewParams);
+            typeView.setLayoutParams(textViewParams);
+            dateView.setLayoutParams(textViewParams);
+            bodyView.setLayoutParams(textViewParams);
+
+            // Set les params du smsContainer
+            LinearLayout.LayoutParams smsViewParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            smsViewParams.setMargins(10, 10, 10, 10);
+            smsContainer.setLayoutParams(smsViewParams);
+            smsContainer.setBackgroundColor(getResources().getColor(R.color.blueAccent));
+            smsContainer.setOrientation(LinearLayout.VERTICAL);
+
+            // Add les textViews au smsContainer
+            smsContainer.addView(numView);
+            smsContainer.addView(typeView);
+            smsContainer.addView(dateView);
+            smsContainer.addView(bodyView);
+
+            // Add du smsContainer a la liste
+            smsListContainer.addView(smsContainer);
+
+            nb_sms++;
+        }
+        if (nb_sms == 0) {
+            TextView errorView = new TextView(this);
+            errorView.setText("No contact sms found second");
+            errorView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+            smsListContainer.addView(errorView);
+        }
+        // Go to the end of the list
+        ScrollView scroll = (ScrollView) findViewById(R.id.smsListScrollView);
+        scroll.scrollTo(0, scroll.getBottom());
     }
 }
